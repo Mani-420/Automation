1ced64e568721fc6715be1b98019329469b3c069
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-08 23:43:50 +0500

    Common Functions of OS

--- OS_Module/Commonly_used_func/common_function.py
+++ OS_Module/Commonly_used_func/common_function.py
@@ -11,3 +11,15 @@ os.rename(fd,'common_function.py')
 # ---------------------------------------------------------
 # Remove file 
 os.remove("F:\Goals\Automation\OS_Module\Commonly_used_func\extra.py")
+
+
+# ---------------------------------------------------------
+# Checking the existing file (a file exist or not)
+result = os.path.exists("Curr_dir.py")
+print(result) 
+
+
+# ---------------------------------------------------------
+# To get the size of the file
+size = os.path.getsize("Curr_dir.py")
+print("Size of the file is", size," bytes.")
