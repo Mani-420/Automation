e9b1fe3072d310e8021b07d25f06e979c33c6656
Mani-420 <tahirabdulrehman46@gmail.com>
2025-01-10 23:37:47 +0500

    Time Module Functions

--- Time_module/main.py
+++ Time_module/main.py
@@ -2,11 +2,20 @@ import time
 # print(time.gmtime(0))
 
 
-# Current time since epoch in seconds
+# Current time since epoch in seconds----------------------------------
 current_time_in_seconds = time.time()
 # print("Current time in seconds since epoch =", current_time_in_seconds)
 
 
-# Current time since epoch in seconds
+# Current time since epoch in seconds----------------------------------
 current_time_till_now = time.ctime()
-print("Current time:", current_time_till_now)
+# print("Current time:", current_time_till_now)
+
+
+# Delay Execution----------------------------------
+# for i in range(4):
+#     time.sleep(1)
+#     print(i)
+
+
+# Delay Execution----------------------------------
