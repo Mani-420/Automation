7478f8b8a4fee1185c42d24ee7659337c78ea1d0
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-23 02:03:02 +0500

    Deleting Files and Folder

---
+++ Files/deleting_files.py
@@ -0,0 +1,9 @@
+import os
+
+# For removing single file --------------------------------------------------
+os.getcwd()
+# os.unlink("f:\Goals\Automation\Files\extra1.txt")
+
+
+# For removing single file --------------------------------------------------
+os.rmdir("f:\Goals\Automation\Files\extrafolder")
